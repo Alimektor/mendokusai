e4443e76ebe49ca238ed788ac33568479d46823c
アリメクタ アレネクト <alimektor@gmail.com>
2024-10-27 16:55:59 +0300

    Add path.sh

--- .config/mendokusai/mendo.sh
+++ .config/mendokusai/mendo.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+# めんどくさい is a public home (~) config directory with useful configs, scripts, cheat codes, shell libraries for Linux / Git Bash for every day.
 
 mendo() {
     /usr/bin/git --git-dir="${HOME}/.config/mendokusai/git" --work-tree="${HOME}" "${@}"
 }
+
+# Customize PATH
+[[ -r "${HOME}/.config/mendokusai/path.sh" ]] && . "${HOME}/.config/mendokusai/path.sh"
---
+++ .config/mendokusai/path.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Customize PATH variable.
+
+# Add local path to the global path.
+pathadd() {
+    local _path="${1}"
+    if [[ -d "${_path}" ]] && [[ ":${PATH}:" != *":${_path}:"* ]]; then
+        export PATH="${PATH:+"${PATH}:"}${_path}"
+    fi
+}
+
+# Add ~/.local/bin to the PATH variable.
+pathadd "${HOME}/.local/bin"
+
+# Add ~/bin to the PATH variable.
+pathadd "${HOME}/bin"
+
+# Add Go binaries to the PATH variable.
+pathadd "${HOME}/go/bin"
+
+# Add Rust binaries and environment to the PATH variable.
+pathadd "${HOME}/.cargo/bin"
+pathadd "${HOME}/.cargo/env"
+
+# Add Snap binaries to the PATH variable.
+pathadd "/var/lib/snapd/snap/bin"
