bac492e4685d8e01c926e4a39223de261ff7df79
アリメクタ アレネクト <alimektor@gmail.com>
2025-02-20 23:35:23 +0300

    Use mendo utility instead of alias

--- .config/mendokusai/mendo.sh
+++ .config/mendokusai/mendo.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 # めんどくさい is a public home (~) config directory with useful configs, scripts, cheat codes, shell libraries for Linux / Git Bash for every day.
 
-mendo() {
-    git --git-dir="${HOME}/.config/mendokusai/git" --work-tree="${HOME}" "${@}"
-}
-
 # Customize PATH
 [[ -r "${HOME}/.config/mendokusai/shell/path.sh" ]] && . "${HOME}/.config/mendokusai/shell/path.sh"
